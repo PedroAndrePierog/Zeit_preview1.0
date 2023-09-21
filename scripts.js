c4b2f25aa2c684cf6411888a8b64bd3a5479f656
PedroAndrePierog <145627589+PedroAndrePierog@users.noreply.github.com>
2023-09-21 00:19:53 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+
+const key = "a58e6b770fd13048870195a61f5cb7dd"
+
+function colocarDadosnaTela(dados){ 
+    
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "% Umidade"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+}
+async function buscarCidade(cidade) {
+    let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+console.log(dados)
+    
+colocarDadosnaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
